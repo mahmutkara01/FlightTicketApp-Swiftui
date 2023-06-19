c02041ead143233f2a9acb5f19f2d5234cfc3ad7
mahmutkara01 <bilgimahmutkara@gmail.com>
2023-06-19 23:55:12 +0300

    Initial Commit

---
+++ karabilet/karabiletApp.swift
@@ -0,0 +1,17 @@
+//
+//  karabiletApp.swift
+//  karabilet
+//
+//  Created by mahmut kara on 19.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct karabiletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
